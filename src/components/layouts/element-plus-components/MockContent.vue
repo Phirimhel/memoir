d3866a81a10e1d825a96031d9ebc6184d0c57d4e
Correct Name <filipp.ardelyan@mvideo.ru>
2025-03-26 23:57:15 +0400

    new collors

---
+++ src/components/layouts/element-plus-components/MockContent.vue
@@ -0,0 +1,13 @@
+<template>
+  <el-input v-model="message" style="width: 240px" placeholder="Please input" />
+  <p v-if="message">{{ message }}</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const message = ref('')
+</script>
+
+<style scoped></style>
+<style></style>
